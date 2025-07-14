9901d45dc22b9b7151d6fccf8616171c3ee1f3f0
mvaranda <m@varanda.ca>
2025-07-14 18:43:38 -0400

    Video 18 antes

--- C-language/v-02/main.c
+++ C-language/v-02/main.c
@@ -5,8 +5,8 @@
 /*
 
  =============
-             #
-             #
+             |
+             |
              O
             \|/
              |
@@ -54,15 +54,6 @@ typedef enum
 char palavra_secreta[128];
 char palavra[128];          // eh mostrado ao jogador, inicialmente : _
 
-void limpa_entrada()
-{
-    char c;
-    do
-    {
-        c = getchar();
-    } while(c != '\n' ||  c == EOF);
-}
-
 void  print_palavra()
 {
     //printf("pavalvra = %s\n", palavra);
@@ -76,56 +67,6 @@ void  print_palavra()
     }
     printf(COR_NORMAL);
     printf("\n");
-
-}
-
-char get_letra()
-{
-    char t[2];
-
-    get_palavra( "Entre com a letra desejada: ", t, 2);
-    return t[0];
-
-}
-
-char get_letra__()
-{
-    char letra;
-    int r;
-
-    printf("Entre com a letra desejada: ");
-    while(1)
-    {
-        int letra_valida = 0; // zero significa invalida
-        r = scanf("%c", &letra);
-        limpa_entrada();
-        if (r == 1)
-        {
-            if (letra == '\n')
-                continue;
-
-            if (letra >= 'A' && letra <= 'Z')
-            {
-                // caracter eh valido e maiusculo
-                // vamos tranformar para minusculo
-                letra = letra + 32;
-                letra_valida = 1;
-            }
-
-            if (letra >= 'a' && letra <= 'z')
-            {
-                letra_valida = 1;
-            }
-
-            if (letra_valida == 1)
-            {
-                return letra;
-            }
-
-            printf("letra invalida, Entre com a letra desejada: \n");
-        }
-
-    }
 
 }
 
@@ -154,26 +95,6 @@ ret_t para_minusculo(char * text)
     return RET_ENTRADA_ACEITA;
 }
 
-ret_t get_palavra_( char * pergunta)
-{
-    int r;
-    printf("%s: ", pergunta);
-    r = scanf("%s", palavra_secreta);
-    if (r == 1)
-    {
-        int i;
-        int len = strlen(palavra_secreta);
-        for (i = 0; i < len; i++ )
-        {
-            palavra[i] = '_';
-        }
-        palavra[i] = 0;
-
-        return RET_ENTRADA_ACEITA;
-    }
-    return RET_ENTRADA_INVALIDA;
-}
-
 ret_t get_palavra( char * pergunta, char * ponterio_texto, int numero_maximo_letras)
 {
     char c;
@@ -185,7 +106,7 @@ ret_t get_palavra( char * pergunta, char * ponterio_texto, int numero_maximo_let
         if (index == numero_maximo_letras)
         {
             printf("Voce digitou mais letras do que esperado...\nTente novamente\n%s", pergunta);
-            while((c = getchar()) != '\n' && c != EOF) ;
+            while((c = getchar()) != '\n' && c != EOF) ;  // limpar buffer de entrada
             index = 0;
             continue;
         }
@@ -223,9 +144,18 @@ ret_t get_palavra( char * pergunta, char * ponterio_texto, int numero_maximo_let
         index = 0;
 
     }
-    ponterio_texto[index++] = 0;
+    ponterio_texto[index] = 0;
 
     return RET_ENTRADA_ACEITA;
+}
+
+char get_letra()
+{
+    char t[2];
+
+    get_palavra( "Entre com a letra desejada: ", t, 2);
+    return t[0];
+
 }
 
 ret_t print_erro(int tentativa)
@@ -314,7 +244,7 @@ void test_4()
     char p[32];
     char c;
 
-    get_palavra("Enter palavra: ", p, 32);
+    get_palavra("Enter palavra: ", p, sizeof(p));
     printf("palavra = %s\n", p);
 
     c = get_letra();
@@ -327,9 +257,6 @@ void test_4()
     printf("letra = %c\n", c);
 
 }
-
-
-
 
 //---------------------------- Main -----------------------------
 
