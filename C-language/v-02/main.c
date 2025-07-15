0c12377faa1152f1beac7836a492570601ca0845
mvaranda <m@varanda.ca>
2025-07-15 10:39:08 -0400

    jogo completo

--- C-language/v-02/main.c
+++ C-language/v-02/main.c
@@ -14,9 +14,6 @@
 
 */
 
-#define MEU_TEXTO "Ola como vai\n\
-Este eh a proxima linha\n"
-
 #define ERRO_1 " =============\n\
              #\n\
              #\n"
@@ -31,16 +28,16 @@ Este eh a proxima linha\n"
 
 #define NUMERO_DE_ERROS 5
 
-#define COR_AMARELO  "\e[33m"
-#define COR_NORMAL   "\e[39m"
+//------ comandos escape para o terminal
+#define COR_AMARELO   "\e[33m"
+#define COR_VERMELHO  "\e[31m"
+#define COR_AZUL      "\e[34m"
+#define COR_NORMAL    "\e[39m"
+#define LIMPA_TELA    "\e[2J"
+#define MOVER_CURSOR "\e[%d;%dH"
 
 char * erros[NUMERO_DE_ERROS] = { ERRO_1, ERRO_2, ERRO_3, ERRO_4, ERRO_5};
 
-//#define RET_ERRO_PROGRAMACAO    -1
-//#define RET_FIM_PERDIDO         1000
-//#define RET_ANDAMENTO           1001
-//#define RET_ENTRADA_INVALIDA    1002
-//#define RET_ENTRADA_ACEITA      1003
 
 typedef enum
 {
@@ -53,6 +50,54 @@ typedef enum
 
 char palavra_secreta[128];
 char palavra[128];          // eh mostrado ao jogador, inicialmente : _
+
+//---------------------------- Windows hack -----------------------------
+#ifdef _WIN32 // mesmo que #if defined(_WIN32)
+#define ENABLE_VIRTUAL_TERMINAL_PROCESSING 4
+#define STD_OUTPUT_HANDLE 0xFFFFFFF5
+#define INVALID_HANDLE_VALUE 0
+typedef int DWORD;
+int GetConsoleMode(void *, DWORD *);
+int SetConsoleMode(void *, DWORD);
+void * GetStdHandle(DWORD);
+int set_terminal()
+{
+    DWORD dwMode = 0;
+    void * hOut;
+
+    hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    if (hOut == INVALID_HANDLE_VALUE)
+    {
+        printf("GetStdHandle error\n");
+        return 1;
+    }
+
+    if (!GetConsoleMode(hOut, &dwMode))
+    {
+        printf("GetConsoleMode error\n");
+        return 1;
+    }
+
+    dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+    if (!SetConsoleMode(hOut, dwMode))
+    {
+        printf("SetConsoleMode error\n");
+        return 1;
+    }
+    return 0;
+}
+#endif // _WIN32
+
+void mover_cursor( int linha, int coluna)
+{
+    printf(MOVER_CURSOR, linha, coluna);
+}
+
+void limpa_tela()
+{
+    printf(LIMPA_TELA);
+    mover_cursor( 1,1);
+}
 
 void  print_palavra()
 {
@@ -70,30 +115,6 @@ void  print_palavra()
 
 }
 
-// aceita apenas caracteres de A~Z ou a~z
-ret_t para_minusculo(char * text)
-{
-    while(*text != 0)
-    {
-        if (*text >= 'A' && *text <= 'Z')
-        {
-            // caracter eh valido e maiusculo
-            // vamos tranformar para minusculo
-            *text = (*text) + 32;
-            text++;
-            continue;
-        }
-
-        if (*text >= 'a' && *text <= 'z')
-        {
-            // caracter eh valido e minusculo
-            text++;
-            continue;
-        }
-        return RET_ENTRADA_INVALIDA;
-    }
-    return RET_ENTRADA_ACEITA;
-}
 
 ret_t get_palavra( char * pergunta, char * ponterio_texto, int numero_maximo_letras)
 {
@@ -168,7 +189,7 @@ ret_t print_erro(int tentativa)
         return RET_ERRO_PROGRAMACAO;
     }
 
-    printf("Errado, tentativa = %d\n\n", tentativa);
+    // printf("Errado, tentativa = %d\n\n", tentativa);
     for (i=0; i <= tentativa - 1; i++)
     {
         printf("%s", erros[i]);
@@ -176,93 +197,124 @@ ret_t print_erro(int tentativa)
 
     if (tentativa >= 5)
     {
-        printf("Fim do jogo, tente novamente\n");
+        // printf("Fim do jogo, tente novamente\n");
         return RET_FIM_PERDIDO;
     }
     return RET_ANDAMENTO;
 }
 
-//-------------------- Unit Test
-int test_1()
-{
-    int i;
 
-    for (i = 1; i <= 5; i++)
-    {
-        printf("\n");
 
-        //printf("sizeof(erros) = %d, sizeof(void  *) = %d\n", sizeof(erros), sizeof(void *));
-        int res = print_erro(i);
 
-        if (res != 0)
-        {
-            printf("jogo em andamente\n");
-        }
-        else
-        {
-            printf("jogo terminou\n");
-        }
-
-        printf("\n");
-    }
-    return 0;
-}
-
-int test_2()
-{
-    ret_t r;
-    r = get_palavra("Digite a palavra secreta: ", palavra_secreta, sizeof(palavra_secreta));
-    if (r == RET_ENTRADA_ACEITA)
-    {
-        printf("A Palavra eh: %s, comprimento = %d caracteres\n", palavra_secreta, (int) strlen(palavra_secreta));
-        r = para_minusculo(palavra_secreta);
-        if (r == RET_ENTRADA_ACEITA)
-        {
-            printf("resultado limpo = %s\n", palavra_secreta);
-        }
-        else
-        {
-            printf("palavra com caracteres nao validos\n");
-        }
-    }
-    else
-    {
-        printf("Entrada invalida\n");
-    }
-
-    return 0;
-}
-
-void test_3()
-{
-    char c = get_letra();
-    printf("Letra escolhida foi: %c\n", c);
-}
-
-void test_4()
-{
-    char p[32];
-    char c;
-
-    get_palavra("Enter palavra: ", p, sizeof(p));
-    printf("palavra = %s\n", p);
-
-    c = get_letra();
-    printf("letra = %c\n", c);
-
-    get_palavra("Enter palavra: ", p, 32);
-    printf("palavra = %s\n", p);
-
-    c = get_letra();
-    printf("letra = %c\n", c);
-
-}
 
 //---------------------------- Main -----------------------------
+#define NAO 0
+#define SIM 1
+
+#define OCULTO '*'
+
+void jogo()
+{
+    int num_erros = 0;
+    int num_tentativas = 0;
+    char c;
+    char tentativas[128];
+    int i;
+    ret_t r;
+    int sair = NAO;
+    int tamanho_da_palavra;
+    int achou_letra;
+
+    memset(tentativas, 0, sizeof(tentativas));
+    memset(palavra_secreta, 0, sizeof(palavra_secreta));
+    memset(palavra, 0, sizeof(palavra));
+
+    get_palavra("Entre com a palavra secreta: ", palavra_secreta, sizeof(palavra_secreta));
+    tamanho_da_palavra = strlen(palavra_secreta);
+    memset(palavra, OCULTO, tamanho_da_palavra);
+
+    while(sair == NAO)
+    {
+        limpa_tela();
+        //------ imprimir erros (se tiver) ------
+        if (num_erros > 0)
+        {
+            r = print_erro(num_erros);
+            if (r != RET_ANDAMENTO)
+            {
+                sair = SIM;
+            }
+        }
+
+        //----- imprimir resultados -------
+        mover_cursor(12,10);
+        printf("Palavra: ");
+        print_palavra();
+        if (num_tentativas > 0)
+        {
+            printf("Tentativas: ");
+            for (i=0; i<num_tentativas; i++)
+            {
+                printf("%c ", tentativas[i]);
+            }
+            printf("\n");
+        }
+        printf("Numero de tentativas: %d\n", num_tentativas);
+
+        //---- caso sair == SIM significa que o jogo foi perdido
+        if (sair == SIM)
+        {
+            printf(COR_VERMELHO "Jogo perdido :-( "  COR_NORMAL "\n");
+            continue;
+        }
+
+        //------- usuario entra letra e verificamos o resultado.
+        achou_letra = NAO;
+        c = get_letra();
+        tentativas[num_tentativas++] = c;
+
+        //---- verifica se letra eh valida ---
+        for (i=0; i<tamanho_da_palavra; i++)
+        {
+            if (c == palavra_secreta[i])
+            {
+                palavra[i] = c;
+                achou_letra = SIM;
+            }
+        }
+
+        if (achou_letra == NAO)
+        {
+            num_erros++;
+        }
+
+        if (strcmp(palavra, palavra_secreta) == 0)
+        {
+            sair = SIM;
+            printf(COR_AZUL "!!! PARABENS !!! voce ganhou o jogo" COR_NORMAL " \n");
+        }
+    }
+
+}
+
 
 int main()
 {
-    test_4();
-    print_palavra();
+#ifdef _WIN32
+    if (set_terminal() != 0)
+    {
+        return 1;
+    }
+#endif // _WIN32
+    char resposta[32];
+
+    do
+    {
+        limpa_tela();
+        printf("        JOGO DA FORCA\n\n");
+        jogo();
+        get_palavra("\nDeseja jogar novamente (sim ou nao) ?\n", resposta, sizeof(resposta));
+    } while (strcmp(resposta, "sim") == 0);
+
     return 0;
 }
