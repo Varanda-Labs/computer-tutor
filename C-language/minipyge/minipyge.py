06c225b2a078928605c0828afec35f3df19385f3
Varanda <m@varanda.ca>
2025-10-29 19:36:02 -0400

    WIP

--- C-language/minipyge/minipyge.py
+++ C-language/minipyge/minipyge.py
@@ -2,16 +2,19 @@ from pyray import *
 import time, sys
 
 class privateNodeBase:
+  # class variables (static)
   all_nodes = []
+  sorted_nodes = None
+
   def __init__(self, name = None):
+    # instance variables
     self.queue = []
     self.name = name
     self.mode3d = False
     self.camera = None
     self.z_pos = 0
-    self.sorted_nodes = None
     if name != None:
-      self.all_nodes.append(self)
+      privateNodeBase.all_nodes.append(self)
   
   def on_init(self):
     pass
@@ -37,51 +40,51 @@ class privateNodeBase:
   def privateSortNodes(self):
     # sort all_nodes based on z_pos
     s = []
-    for n in self.all_nodes:
+    for n in privateNodeBase.all_nodes:
       s.append([n.z_pos, n])
     ss = sorted(s, key=lambda x: x[0], reverse=True)
-    self.sorted_nodes = []
+    privateNodeBase.sorted_nodes = []
     for n in ss:
-      self.sorted_nodes.append(n[1])
+      privateNodeBase.sorted_nodes.append(n[1])
 
   def privateRun(self):
 
-    for n in self.all_nodes:
+    for n in privateNodeBase.all_nodes:
       n.on_init()
 
     self.privateSortNodes()
 
     while not window_should_close():
       #------ Dispatch messages ------ 
-      for n in self.sorted_nodes:
+      for n in privateNodeBase.sorted_nodes:
         while len(n.queue) > 0:
           n.on_message(n.queue[0], get_frame_time())
           del(n.queue[0])
 
       #------ on_slice's -------
-      for n in self.sorted_nodes:
+      for n in privateNodeBase.sorted_nodes:
         n.on_slice(get_frame_time())
 
       #------ on_draw_canvas's -------
       begin_drawing()
-      for n in self.sorted_nodes:
+      for n in privateNodeBase.sorted_nodes:
         n.on_draw_canvas(get_frame_time())
 
       #------ on_draw_2d's or on_draw_3d -------
       if self.mode3d == False:
         begin_mode_2d(self.camera)
-        for n in self.sorted_nodes:
+        for n in privateNodeBase.sorted_nodes:
           n.on_draw_2d(get_frame_time())
         end_mode_2d()
       else:
         begin_mode_3d(self.camera)
-        for n in self.sorted_nodes:
+        for n in privateNodeBase.sorted_nodes:
           n.on_draw_3d(get_frame_time())
         end_mode_2d()
       end_drawing()
 
     #----- destruction 
-    for n in self.all_nodes:
+    for n in privateNodeBase.all_nodes:
       n.on_destroy()
       
   def send_message(self, node_dst, obj, node_from = None):
