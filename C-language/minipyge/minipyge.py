75a599af22ee88373cb06d45baf9882171381035
Varanda <m@varanda.ca>
2025-10-30 20:34:13 -0400

    add comments to minipyge

--- C-language/minipyge/minipyge.py
+++ C-language/minipyge/minipyge.py
@@ -1,7 +1,17 @@
+"""MiniPyGE - Mini Python Game Engine
+
+Tiny set of classes and methods to make raylib based games organized.
+The classes of the game can be derived from MiniGNode. Callbacks methods are
+ automatically called to perform basic operations like draw.
+"""
+
 from pyray import *
 import time, sys
 
 class privateNodeBase:
+  """
+  private class. Game should not use this class
+  """
   # class variables (static)
   all_nodes = []
   sorted_nodes = None
@@ -36,6 +46,10 @@ class privateNodeBase:
 
   def on_destroy(self):
     pass
+
+  def send_message(self, node_dst, obj, node_from = None):
+    node_dst.queue.append([obj, node_from])
+
 
     # static member
   def privateSortNodes():
@@ -90,18 +104,65 @@ class privateNodeBase:
     #----- destruction 
     for n in privateNodeBase.all_nodes:
       n.on_destroy()
-      
-  def send_message(self, node_dst, obj, node_from = None):
-    node_dst.queue.append([obj, node_from])
 
 
 
 class MiniGNode(privateNodeBase):
+  """
+    MiniGNode is the class which all game classes should be derived from.
+  """
   def __init__(self, name):
     if name == None:
       print("MiniGNode: Name must be provided")
       sys.exit(1)
     super().__init__(name)
+
+  def on_init(self):
+    """
+      on_init: is called only once during initialization.
+      The class should use this method to use raylib function like loading images, textures, models and etc.
+      Note: The object constructor, the __init___ method, should not be used to call raylib functions. 
+    """
+    pass
+
+  def on_slice(self, timestamp):
+    """
+      on_slice is called from the core loop right before on_draw_* being called.
+      This is the place to do all logic operations prior to render each frame.
+    """
+    pass
+
+  def on_draw_canvas(self, timestamp):
+    """
+      on_draw_canvas: proper method to draw 2D elements to the screen. 
+      Usually UI screen overlays where camera position has ne affect.
+    """
+    pass
+
+  def on_draw_2d(self, timestamp):
+    """
+      on_draw_2d: the core calls raylib's begin_mode_2d(self.camera) method prior calling this callback
+      Note: the game code should have called minipyge_set_2d during initialization to provide the camera object.
+    """
+    pass
+
+  def on_draw_3d(self, timestamp):
+    """
+      on_draw_3d: the core calls raylib's begin_mode_3d(self.camera) method prior calling this callback
+      Note: the game code should have called minipyge_set_3d during initialization to provide the camera object.
+    """
+    pass
+
+  def on_message(self, msg, timestamp):
+    """
+      on_message: any derived class can call send_message method passing as parameters:
+      destination node and object.
+      The on_message is called to deliver the object. msg is a list with two elements:
+      the first is the sent object and the second element is the sender.
+    """
+    pass
+
+##----------------- Functions -------------------
 
 g_minipyge_root_node = privateNodeBase()
 
